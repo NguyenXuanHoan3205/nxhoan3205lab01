e2a92333715379add8a1ce7c00bdf25bc564dd2a
NguyenXuanHoan3205 <honanguyen2005@gmail.com>
2024-09-03 21:29:22 +0700

    Add files via upload

---
+++ bai13.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 2 18:55:04 2024
+
+@author: Nguyễn Xuân Hoan - 23694771
+"""
+
+day = int(input("Nhập ngày: "))
+month = int(input("Nhập tháng: "))
+year = int(input("Nhập năm: "))
+d = year%100
+print("a> Ngày/tháng/năm: ",f"{day}/{month}/{year}")
+print("b> Ngày/tháng/năm: ",f"{day}/{month}/{d}")
+print("c> Năm/tháng/ngày: ",f"{year}/{month}/{day}")
+    
